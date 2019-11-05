13edbe7a4ad904d06629ff5b3e3b63659b333124
dwiyenss <dsamuel.saragih@gmail.com>
2019-11-05 13:54:56 +0700

    shopping cart quasar

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -33,6 +33,82 @@
         </template>
       </q-input>
     </div>
+    <!-- Keranjang -->
+    <q-btn flat round icon="shopping_cart" color="white" text-color="black" @click="fixed = true"  />
+
+      <q-dialog v-model="fixed">
+      <q-card style="width:700px">
+        <q-toolbar class="bg-primary text-black shadow-2">
+      <q-toolbar-title>Tabel Barang</q-toolbar-title>
+    </q-toolbar>
+
+    <q-list bordered class="rounded-borders bg-white">
+      <q-item class="bg-teal">
+        <q-item-section avatar top class="col-1 gt-xm" style="align : left">
+            <q-item-label class="q-mt-sm">No</q-item-label>
+        </q-item-section>
+
+        <q-item-section top class="col-2   gt-xm" style="align : left">
+            <q-item-label class="q-mt-sm">Nama Barang</q-item-label>
+        </q-item-section>
+
+        <q-item-section top class="col-1 gt-xm" style="align : center">
+            <q-item-label class="q-mt-sm">Jumlah</q-item-label>
+        </q-item-section>
+        
+        <q-item-section top class="col-2 gt-xm" style="align : center">
+            <q-item-label class="q-mt-sm">Harga</q-item-label>
+        </q-item-section>
+
+        <q-item-section top class="col-3 gt-xm" style="align : center">
+            <q-item-label class="q-mt-sm">Keterangan</q-item-label>
+        </q-item-section>
+
+        <q-item-section top class="col-2 gt-xm">
+            <q-item-label class="q-mt-sm flex flex-center">Aksi</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item>
+        <q-item-section avatar top class="col-1 gt-xm">
+            <q-item-label class="q-mt-sm">1</q-item-label>
+        </q-item-section>
+
+        <q-item-section top class="col-2   gt-xm" style="align : left">
+            <q-item-label class="q-mt-sm">Panci</q-item-label>
+        </q-item-section>
+
+        <q-item-section top class="col-1 gt-xm" style="align : center">
+            <q-item-label class="q-mt-sm">90</q-item-label>
+        </q-item-section>
+        
+        <q-item-section top class="col-2 gt-xm" style="align : center">
+            <q-item-label class="q-mt-sm">Rp 750000</q-item-label>
+        </q-item-section>
+
+        <q-item-section top class="col-3 gt-xm" style="align : center">
+            <q-item-label class="q-mt-sm">warna kuning-silver</q-item-label>
+        </q-item-section>
+
+        <q-item-section top class="col-2 gt-xm">
+          <div class="q-mt-sm flex flex-center" style="align : right">
+            <q-btn class="gt-xs" size="12px" flat dense round icon="delete" />
+            <q-btn class="gt-xs" size="12px" flat dense round icon="done" />
+            <q-btn size="12px" flat dense round icon="more_vert" />
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Decline" color="blue" v-close-popup />
+          <q-btn flat label="Accept" color="blue" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <!-- akhir Keranjang -->
+
         <q-btn style= "color : black" label="Login"  flat class="q-ms-sm">
           <q-menu  
           transition-show="rotate"
@@ -275,7 +351,7 @@ export default {
       username: "",
       password: "",
       isPwd: true,
-
+      fixed: false,
       accept: false
     }
   },
